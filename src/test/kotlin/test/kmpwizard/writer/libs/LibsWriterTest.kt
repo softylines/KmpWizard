b739d8f00ad2c647539bdc57546a2c3c373e100f
MohamedRejeb <mohamedrejeb445@gmail.com>
2025-01-30 12:18:59 +0100

    Create ConventionPluginWriter

--- src/test/kotlin/test/kmpwizard/writer/libs/LibsWriterTest.kt
+++ src/test/kotlin/test/kmpwizard/writer/libs/LibsWriterTest.kt
@@ -5,6 +5,7 @@ import com.softylines.kmpwizard.writer.libs.LibsWriter
 import org.junit.Before
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LibsWriterTest {
 
@@ -86,6 +87,29 @@ class LibsWriterTest {
         }
 
         assertEquals(expectedNewLines, newLines)
+    }
+
+    @Test
+    fun `test write version and versions block`() {
+        val lines = listOf(
+            "[libraries]",
+            "kotlin = { module = 'org.jetbrains.kotlin:kotlin-stdlib-jdk8', version = '1.0.0' }",
+            "",
+            "[plugins]",
+            "kotlin = { id = 'org.jetbrains.kotlin.jvm', version = '1.0.0' }"
+        )
+
+        val expectedNewLine = "compose = \"1.7.3\""
+
+        val newLines = LibsWriter.writeVersion(
+            version = LibsLine.Version("compose", "1.7.3"),
+            lines = lines
+        )
+
+        assertEquals("[versions]", newLines[newLines.lastIndex - 1])
+        assertEquals(expectedNewLine, newLines[newLines.lastIndex])
+        assertEquals(lines.size + 2, newLines.size)
+        assertEquals(lines, newLines.dropLast(2))
     }
 
 }
